fe2995efdb11800d8013c5a611cb121a990ee9f2
Kal <info@kalacademy.org>
2020-06-20 16:11:01 -0700

    Catalog API

--- ProductCatalogAPI/Controllers/CatalogController.cs
+++ ProductCatalogAPI/Controllers/CatalogController.cs
@@ -5,7 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using ProductCatalogAPI.Data;
+using ProductCatalogAPI.Domain;
+using ProductCatalogAPI.ViewModels;
 
 namespace ProductCatalogAPI.Controllers
 {
@@ -14,9 +17,11 @@ namespace ProductCatalogAPI.Controllers
     public class CatalogController : ControllerBase
     {
         private readonly CatalogContext _context;
-        public CatalogController(CatalogContext context)
+        private readonly IConfiguration _config;
+        public CatalogController(CatalogContext context, IConfiguration config)
         {
             _context = context;
+            _config = config;
         }
 
         [HttpGet("[action]")]
@@ -24,13 +29,34 @@ namespace ProductCatalogAPI.Controllers
             [FromQuery]int pageIndex = 0,
             [FromQuery]int pageSize = 6)
         {
+            var itemsCount = _context.CatalogItems.LongCountAsync();
+
             var items = await _context.CatalogItems
                             .OrderBy(c => c.Name)
                             .Skip(pageIndex * pageSize)
                             .Take(pageSize)
                             .ToListAsync();
 
-            return Ok(items);
+            items = ChangePictureUrl(items);
+
+            var model = new PaginatedItemsViewModel<CatalogItem>
+            {
+                PageIndex = pageIndex,
+                PageSize = items.Count,
+                Count = itemsCount.Result,
+                Data = items
+            };
+
+            return Ok(model);
+        }
+
+        private List<CatalogItem> ChangePictureUrl(List<CatalogItem> items)
+        {
+            items.ForEach(item =>
+                item.PictureUrl = item.PictureUrl.Replace(
+                                    "http://externalcatalogbaseurltobereplaced",
+                                    _config["ExternalCatalogBaseUrl"]));
+            return items;
         }
     }
 }
