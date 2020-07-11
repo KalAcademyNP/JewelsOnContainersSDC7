b9e8f269cb5aad982ccbc83e26121c68696229bd
Kal <info@kalacademy.org>
2020-07-11 16:09:46 -0700

    More webmvc code

---
+++ WebMvc/Controllers/CatalogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebMvc.Services;
+
+namespace WebMvc.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly ICatalogService _service;
+        public CatalogController(ICatalogService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index(int? page, int? brandFilterApplied, int? typesFilterApplied)
+        {
+            var itemsOnPage = 10;
+
+            var catalog = await _service.GetCatalogItemsAsync(page ?? 0, itemsOnPage, brandFilterApplied, typesFilterApplied);
+
+            return View();
+        }
+    }
+}
--- WebMvc/Infrastructure/CustomHttpClient.cs
+++ WebMvc/Infrastructure/CustomHttpClient.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace WebMvc.Infrastructure
 {
     public class CustomHttpClient : IHttpClient
     {
-        public Task<string> GetStringAsync(string uri, 
+        private readonly HttpClient _client;
+        public CustomHttpClient()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<string> GetStringAsync(string uri, 
             string authorizationToken = null, 
             string authorizationMethod = "Bearer")
         {
-            throw new NotImplementedException();
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
+
+            var response = await _client.SendAsync(requestMessage);
+            return await response.Content.ReadAsStringAsync();
+
         }
     }
 }
