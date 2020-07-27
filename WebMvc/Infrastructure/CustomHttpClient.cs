6dfd54aa9734bb323e802a9c8ac53cb175f181af
Kal <info@kalacademy.org>
2020-07-27 10:46:25 -0700

    Cart integration

--- WebMvc/Infrastructure/CustomHttpClient.cs
+++ WebMvc/Infrastructure/CustomHttpClient.cs
@@ -24,6 +24,10 @@ namespace WebMvc.Infrastructure
             string authorizationMethod = "Bearer")
         {
             var requestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
+            if (authorizationToken != null)
+            {
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue(authorizationMethod, authorizationToken);
+            }
 
             var response = await _client.SendAsync(requestMessage);
             return await response.Content.ReadAsStringAsync();
@@ -77,12 +81,9 @@ namespace WebMvc.Infrastructure
         public async Task<HttpResponseMessage> DeleteAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer")
         {
             var requestMessage = new HttpRequestMessage(HttpMethod.Delete, uri);
-            // SetAuthorizationHeader(requestMessage);
             if (authorizationToken != null)
             {
-
                 requestMessage.Headers.Authorization = new AuthenticationHeaderValue(authorizationMethod, authorizationToken);
-
             }
 
             return await _client.SendAsync(requestMessage);
